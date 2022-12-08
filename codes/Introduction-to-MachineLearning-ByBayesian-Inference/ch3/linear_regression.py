9871ad958fabdea4959194c43d41c2f829f6ac32
tsugumi-sys <tidemark0105@gmail.com>
2022-12-08 11:30:56 +0900

    add reg

--- codes/Introduction-to-MachineLearning-ByBayesian-Inference/ch3/linear_regression.py
+++ codes/Introduction-to-MachineLearning-ByBayesian-Inference/ch3/linear_regression.py
@@ -1,11 +1,33 @@
-from typing import Tuple
+from typing import Tuple, List
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def softmax(x: np.ndarray) -> np.ndarray:
-    return np.exp(x) / np.sum(np.exp(x), axis=0)
+# def softmax(x: np.ndarray) -> np.ndarray:
+#     return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
+class LinearRegression:
+    def __init__(self, ndim: int, error_std: float) -> None:
+        self.ndim = ndim
+        self.error_std = error_std
+        self.error = np.random.normal(0, 1 / error_std, 1)
+        self.mean = np.zeros((ndim))
+        self.vc_matrix = np.diag(np.ones((ndim)))
+        self.weights = np.random.multivariate_normal(self.mean, self.vc_matrix)
+
+    def forward(self, x) -> float:
+        return np.dot(self.weights, x) + self.error
+
+    def update(self, inputs: List[Tuple]) -> None:
+        """
+        Args:
+            inputs (List[Tuple]): Lists of (x, y)
+        """
+        sum_x = 0
+        for x, _ in inputs:
+            sum_x += np.dot(x, x.T)
 
 
 def linear_regression(ndim: int, error_std: float) -> Tuple:
@@ -14,7 +36,6 @@ def linear_regression(ndim: int, error_std: float) -> Tuple:
     m = np.zeros((ndim))
     A = np.diag(np.ones((ndim)))
     w = np.random.multivariate_normal(m, A)
-    # w = softmax(w)
 
     return w, e
 
